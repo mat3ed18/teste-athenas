be365e11f339f99b7decc60b77cbb6e464aee0f2
mat3ed18 <mathiaspedro9@gmail.com>
2021-11-03 22:50:23 -0300

    adicionando o css/js e as páginas de cadastro e listagem

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
         <title>CRUD Athenas</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="source/css/estilo.css"/>
+        <script type="text/javascript" src="jquery/jquery-3.6.0.min.js"></script>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script type="text/javascript" src="source/js/script.js"></script>
+        <script src="https://gist.github.com/pedropuppim/fd15c4d0ecd766873c721407df0ac2f9.js"></script>
     </head>
     <body>
-        <?php
-            echo "Teste";
-        ?>
+        <ul class="nav justify-content-center shadow bg-dark">
+            <li class="nav-item">
+                <a class="nav-link active" href="cadastro/">Cadastro</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="pessoas/">Usuários</a>
+            </li>
+        </ul>
+        
+        <form action="../teste-athenas/" method="post">
+            
+        </form>
     </body>
 </html>
---
+++ source/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    // codigo javascript
+    
+    $("#gist95989549").addClass("shadow");
+    
+    $(".nav").after($("#gist95989549").detach());
+});
